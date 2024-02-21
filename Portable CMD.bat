c2b3547f3939a552e1da8582b689fb89c677c147
Tactics <76490725+tactics-osrs@users.noreply.github.com>
2024-02-21 18:08:29 -0500

    Create Portable CMD.bat

---
+++ Portable CMD.bat
@@ -0,0 +1,16 @@
+@echo off
+
+REM Script Name: Portable CMD
+REM Date Written: February 18, 2024
+REM Written By: Tactics
+
+
+cls
+title Portable CMD  %cd%
+color 0a
+cls
+:cmd
+set /p "cmd=%CD%>"
+%cmd%
+echo.
+goto cmd
